23d5386ab1557a03d6573df7791e3b7ee001a0e0
gxz2019 <1173017852@qq.com>
2020-03-16 10:00:29 +0800

    recommend组件header的完成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { GlobalStyle } from './style'
+import { IconStyle } from './assets/iconfont/iconfont'
+import { renderRoutes } from 'react-router-config'  //读取路由配置转换成route标签
+import routes from './routes/index'
+import {HashRouter} from 'react-router-dom'
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <GlobalStyle />
+        <IconStyle />
+        {renderRoutes(routes)}
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
